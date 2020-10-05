c34fe13809c6dd0518d212b117582fac8fc69071
Michiel <michiellof@gmail.com>
2020-10-05 19:00:58 +0200

    Lesson 322

--- 31 - RESTful Routing/RESTfulBlogApp/app.js
+++ 31 - RESTful Routing/RESTfulBlogApp/app.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     mongoose = require("mongoose"),
     express = require("express"),
     app = express();
@@ -14,6 +15,7 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 //
 // MONGOOSE/MODEL CONFIG
@@ -53,6 +55,7 @@ app.get("/blogs/new", function (req, res) {
 // CREATE ROUTE
 app.post("/blogs", function (req, res) {
     //create blog
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.create(req.body.blog, function (err, newBlog) {
         if (err) {
             res.render("new");
@@ -87,6 +90,7 @@ app.get("/blogs/:id/edit", function (req, res) {
 
 // UPDATE ROUTE
 app.put("/blogs/:id", function (req, res) {
+    req.body.blog.body = req.sanitize(req.body.blog.body);
     Blog.findByIdAndUpdate(req.params.id, req.body.blog, function (err, updatedBlog) {
         if (err) {
             res.redirect("/blogs");
