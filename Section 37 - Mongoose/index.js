66005d401371f6ac14043e6d028bc5744179877d
Michiel <michiellof@gmail.com>
2020-11-23 15:47:07 +0100

    Lesson 380

--- Section 37 - Mongoose/index.js
+++ Section 37 - Mongoose/index.js
@@ -7,3 +7,14 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log("OH NO! ANYWAY...");
         console.log(err);
     });
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
