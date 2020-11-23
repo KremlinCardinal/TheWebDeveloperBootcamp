7d42060f0067323b91d34066c2df2bc558aa0b1a
Michiel <michiellof@gmail.com>
2020-11-23 21:10:51 +0100

    Lesson 381

--- Section 37 - Mongoose/index.js
+++ Section 37 - Mongoose/index.js
@@ -17,4 +17,15 @@ const movieSchema = new mongoose.Schema({
 
 const Movie = mongoose.model('Movie', movieSchema);
 
-const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+// const amadeus = new Movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
+
+Movie.insertMany([
+    { title: 'Amelie', year: 2001, score: 8.3, rating: 'R' },
+    { title: 'Alien', year: 1979, score: 8.1, rating: 'R' },
+    { title: 'The Iron Giant', year: 1999, score: 7.5, rating: 'PG' },
+    { title: 'Stand By Me', year: 1986, score: 8.6, rating: 'R' },
+    { title: 'Moonrise Kingdom', year: 2012, score: 7.3, rating: 'PG-13' }
+]).then(data => {
+    console.log("it worked!");
+    console.log(data);
+})
