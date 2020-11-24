37325cf8df9ae43c58bdd37c23ac4b1990fec6c4
Michiel <michiellof@gmail.com>
2020-11-24 21:12:36 +0100

    Lesson 386

--- Section 37 - Mongoose/product.js
+++ Section 37 - Mongoose/product.js
@@ -11,17 +11,34 @@ mongoose.connect('mongodb://localhost:27017/shopApp', { useNewUrlParser: true, u
 const productSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        maxlength: 20,
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        min: 0
+    },
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    categories: [String],
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        inStore: {
+            type: Number,
+            default: 0
+        }
     }
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-const bike = new Product({ name: 'Mountain Bike', price: 999, color: 'red' });
+const bike = new Product({ name: 'Bike Helmet', price: 19.50, categories: ['Cycling', 'Safety'] });
 bike.save()
     .then(data => {
         console.log("IT WORKED!");
