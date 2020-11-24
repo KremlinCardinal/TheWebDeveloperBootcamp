61501e2848ffea9b8405aa239b94e58aac2a8fc2
Michiel <michiellof@gmail.com>
2020-11-24 21:21:58 +0100

    Lessson 388

--- Section 37 - Mongoose/product.js
+++ Section 37 - Mongoose/product.js
@@ -17,7 +17,7 @@ const productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true,
-        min: 0
+        min: [0, 'Price must be positive']
     },
     onSale: {
         type: Boolean,
@@ -33,23 +33,17 @@ const productSchema = new mongoose.Schema({
             type: Number,
             default: 0
         }
+    },
+    size: {
+        type: String,
+        enum: ['S', 'M', 'L']
     }
 });
 
 const Product = mongoose.model('Product', productSchema);
 
-// const bike = new Product({ name: 'Tire Pump', price: 19.50, categories: ['Cycling'] });
-// bike.save()
-//     .then(data => {
-//         console.log("IT WORKED!");
-//         console.log(data);
-//     })
-//     .catch(err => {
-//         console.log("OH NO! ANYWAY...");
-//         console.log(err);
-//     });
-
-Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9.99 }, { new: true, runValidators: true })
+const bike = new Product({ name: 'Cycling Jersey', price: 28.50, categories: ['Cycling'], size: 'XS' });
+bike.save()
     .then(data => {
         console.log("IT WORKED!");
         console.log(data);
@@ -58,3 +52,13 @@ Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9.99 }, { new: true, r
         console.log("OH NO! ANYWAY...");
         console.log(err);
     });
+
+// Product.findOneAndUpdate({ name: 'Tire Pump' }, { price: -9.99 }, { new: true, runValidators: true })
+//     .then(data => {
+//         console.log("IT WORKED!");
+//         console.log(data);
+//     })
+//     .catch(err => {
+//         console.log("OH NO! ANYWAY...");
+//         console.log(err);
+//     });
