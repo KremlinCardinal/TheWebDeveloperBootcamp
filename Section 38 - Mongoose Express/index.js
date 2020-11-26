91c6f5ea79abf7860057c71f5e099258612719a0
Michiel <michiellof@gmail.com>
2020-11-26 11:12:12 +0100

    Lesson 396: Products index

--- Section 38 - Mongoose Express/index.js
+++ Section 38 - Mongoose Express/index.js
@@ -17,8 +17,13 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.get('/dog', (req, res) => {
-    res.send('Woof');
+/* ************** */
+/*     ROUTES     */
+/* ************** */
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({});
+    res.render('products/index', { products });
 });
 
 app.listen(3000, () => {
