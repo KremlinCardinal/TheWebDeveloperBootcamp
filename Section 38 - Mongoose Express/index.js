42e1aabe6262a1417366fcb9f50459dc76499d75
Michiel <michiellof@gmail.com>
2020-11-26 11:24:34 +0100

    Lesson 397: Product details

--- Section 38 - Mongoose Express/index.js
+++ Section 38 - Mongoose Express/index.js
@@ -26,6 +26,12 @@ app.get('/products', async (req, res) => {
     res.render('products/index', { products });
 });
 
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product });
+});
+
 app.listen(3000, () => {
     console.log('App is listening on port 3000');
 });
