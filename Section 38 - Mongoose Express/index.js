8b7c63898498155e0d11a0400198426475ce81f8
Michiel <michiellof@gmail.com>
2020-11-26 15:36:43 +0100

    Lesson 400

--- Section 38 - Mongoose Express/index.js
+++ Section 38 - Mongoose Express/index.js
@@ -20,6 +20,8 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
+const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
+
 /* ************** */
 /*     ROUTES     */
 /* ************** */
@@ -30,7 +32,7 @@ app.get('/products', async (req, res) => {
 });
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
+    res.render('products/new', { categories });
 });
 
 app.post('/products', async (req, res) => {
@@ -49,7 +51,7 @@ app.get('/products/:id', async (req, res) => {
 app.get('/products/:id/edit', async (req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product });
+    res.render('products/edit', { product, categories });
 });
 
 app.put('/products/:id', async (req, res) => {
