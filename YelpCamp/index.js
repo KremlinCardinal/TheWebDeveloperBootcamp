0033777a7c6ca2d8ccbadbcfcf134c41791a99a2
Michiel <michiellof@gmail.com>
2020-11-27 11:01:48 +0100

    Lesson 406: Campground Model Basics

--- YelpCamp/index.js
+++ YelpCamp/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+
+
+/* ************** */
+/*     ROUTES     */
+/* ************** */
+
 app.get('/', (req, res) => {
     res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My Backyard', description: 'Cheap camping' });
+    await camp.save();
+    res.send(camp);
 });
 
 app.listen(3000, () => {
