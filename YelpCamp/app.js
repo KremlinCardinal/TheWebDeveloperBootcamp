89f3bc8d3118e220f3f00edc8a295fef626254e8
Michiel <michiellof@gmail.com>
2020-11-27 19:27:31 +0100

    Lesson 410: Campground new & create

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -21,6 +21,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({ extended: true }));
+
 
 
 /* ************** */
@@ -34,6 +36,16 @@ app.get('/', (req, res) => {
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
