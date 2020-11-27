47ac79beca519d21e08f0ffdab0b10c6c66ee87c
Michiel <michiellof@gmail.com>
2020-11-27 12:43:52 +0100

    Lesson 408: Campgrounds index

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -31,10 +31,9 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My Backyard', description: 'Cheap camping' });
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
 });
 
 app.listen(3000, () => {
