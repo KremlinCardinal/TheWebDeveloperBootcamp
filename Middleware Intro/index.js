77105b0b7c79bf2eb2e707d05fe8e64edde19b64
Michiel Dijk <michiellof@gmail.com>
2020-11-30 12:24:13 +0100

    Lesson 417: More middleware practice

--- Middleware Intro/index.js
+++ Middleware Intro/index.js
@@ -2,13 +2,21 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+});
 
 app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('Home page');
 });
 
 app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
     res.send('WOOF WOOF!');
 });
 
