c8617b20a1130d1b083b0858961b3b46f7e50ed4
Michiel Dijk <michiellof@gmail.com>
2020-12-01 20:29:48 +0100

    Lesson 434: Handling async errors

---
+++ Error Handling in Express/async/index.js
@@ -0,0 +1,87 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+const methodOverride = require('method-override');
+const AppError = require('./AppError');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
+        console.log("Mongo connection open...");
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!");
+        console.log(err);
+    });
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+const categories = ['fruit', 'vegetable', 'dairy', 'fungi'];
+
+/* ************** */
+/*     ROUTES     */
+/* ************** */
+
+app.get('/products', async (req, res) => {
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All' });
+    }
+});
+
+app.get('/products/new', (req, res) => {
+    throw new AppError('Not allowed', 401);
+    res.render('products/new', { categories });
+});
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    console.log(newProduct);
+    res.redirect(`/products/${newProduct._id}`);
+})
+
+app.get('/products/:id', async (req, res, next) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    if (!product) {
+        return next(new AppError('Product not found', 404));
+    }
+    res.render('products/show', { product });
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product, categories });
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+
+    res.redirect(`/products/${product._id}`);
+});
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
+});
+
+app.use((err, req, res, next) => {
+    const { status = 500, message = 'Something went wrong ' } = err;
+    res.status(status).send(message);
+});
+
+app.listen(3000, () => {
+    console.log('App is listening on port 3000');
+});
