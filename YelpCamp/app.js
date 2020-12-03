15453d6254c465432a213b2dd77b798883b2b2aa
Michiel Dijk <michiellof@gmail.com>
2020-12-03 16:10:00 +0100

    Lesson 442

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,6 +5,7 @@ const ejsMate = require('ejs-mate');
 const catchAsync = require('./utils/catchAsync');
 const methodOverride = require('method-override');
 const Campground = require('./models/campground');
+const ExpressError = require('./utils/ExpressError');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
@@ -48,6 +49,7 @@ app.get('/campgrounds/new', (req, res) => {
 });
 
 app.post('/campgrounds', catchAsync(async (req, res, next) => {
+    if (!req.body.campground) throw new ExpressError('Invalid campground data', 400);
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
@@ -75,8 +77,14 @@ app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     res.redirect('/campgrounds');
 }));
 
+/* 404 ROUTE */
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page not found', 404));
+});
+
 app.use((err, req, res, next) => {
-    res.send('Oh no! Something went wrong!');
+    const { statusCode = 500, message = 'Something went wrong' } = err;
+    res.status(statusCode).send(message);
 });
 
 app.listen(3000, () => {
