0647b35853e29990eaa98aebbb3fa2a93a7e10c7
Michiel Dijk <michiellof@gmail.com>
2020-12-07 14:56:34 +0100

    Lesson 455

---
+++ Section 45 - Mongo Relationships with Express/models/farm.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const farmSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Farm must have a name']
+    },
+    city: {
+        type: String
+    },
+    email: {
+        type: String,
+        required: [true, 'Email required']
+    },
+    products: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        }
+    ]
+});
+
