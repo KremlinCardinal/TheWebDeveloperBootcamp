4cec88c27a2715d50397d5ff2252c4c23589b9dc
Michiel Dijk <michiellof@gmail.com>
2020-12-08 09:44:13 +0100

    Lesson 459

--- Section 45 - Mongo Relationships with Express/index.js
+++ Section 45 - Mongo Relationships with Express/index.js
@@ -30,7 +30,7 @@ app.get('/farms', async (req, res) => {
 });
 
 app.get('/farms/:id', async (req, res) => {
-    const farm = await Farm.findById(req.params.id);
+    const farm = await Farm.findById(req.params.id).populate('products');
     res.render('farms/show', { farm });
 });
 
@@ -44,9 +44,10 @@ app.post('/farms', async (req, res) => {
     res.redirect('/farms');
 });
 
-app.get('/farms/:id/products/new', (req, res) => {
+app.get('/farms/:id/products/new', async (req, res) => {
     const { id } = req.params;
-    res.render('products/new', { categories, id });
+    const farm = await Farm.findById(id);
+    res.render('products/new', { categories, farm });
 });
 
 app.post('/farms/:id/products', async (req, res) => {
@@ -58,7 +59,7 @@ app.post('/farms/:id/products', async (req, res) => {
     product.farm = farm;
     await farm.save();
     await product.save();
-    res.send(farm);
+    res.redirect(`/farms/${id}`);
 });
 
 // PRODUCT ROUTES
@@ -88,7 +89,7 @@ app.post('/products', async (req, res) => {
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
-    const product = await Product.findById(id);
+    const product = await Product.findById(id).populate('farm', 'name');
     res.render('products/show', { product });
 });
 
