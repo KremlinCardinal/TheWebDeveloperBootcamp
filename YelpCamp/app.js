b1805c623de9bc0770f31d0da12e24bc13b8c450
Michiel Dijk <michiellof@gmail.com>
2020-12-11 16:15:32 +0100

    Lesson 467: Deleting reviews

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -107,6 +107,13 @@ app.post('/campgrounds/:id/reviews', validateReview, catchAsync(async (req, res)
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+    res.redirect(`/campgrounds/${id}`);
+}));
+
 /* 404 ROUTE */
 app.all('*', (req, res, next) => {
     next(new ExpressError('Page not found', 404));
