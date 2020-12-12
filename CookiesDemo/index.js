53cdfc0f3903c6e4474169fcfb30344f33e88c73
Michiel Dijk <michiellof@gmail.com>
2020-12-12 11:06:08 +0100

    Lesson 474: Cookie parser middleware

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(cookieParser());
+
 app.get('/greet', (req, res) => {
-    res.send('hey there');
+    const { name = 'No-name' } = req.cookies;
+    res.send(`Hey there ${name}`);
 });
 
 app.get('/setname', (req, res) => {
