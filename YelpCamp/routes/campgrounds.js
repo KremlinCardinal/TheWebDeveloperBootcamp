a96fb55544bde0fdcb9e403993ef16ed554886ba
Michiel Dijk <michiellof@gmail.com>
2020-12-14 20:39:29 +0100

    Lesson 487: Setting up flash

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -30,6 +30,7 @@ router.get('/new', (req, res) => {
 router.post('/', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground!');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
