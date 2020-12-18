fb38cbdd0bcba1e1d52b17677c6377fc6765c618
Michiel Dijk <michiellof@gmail.com>
2020-12-18 15:54:51 +0100

    Lesson 498: Auth demo - login

--- Section 50 - Authentication From Scratch/AuthDemo/index.js
+++ Section 50 - Authentication From Scratch/AuthDemo/index.js
@@ -37,6 +37,22 @@ app.post('/register', async (req, res) => {
     res.redirect('/');
 });
 
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    const validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+        res.send('password correct');
+    } else {
+        res.send('try again');
+    }
+});
+
+
 app.get('/secret', (req, res) => {
     res.send('This page is secret! You cannot see me unless you\'re logged in!');
 });
