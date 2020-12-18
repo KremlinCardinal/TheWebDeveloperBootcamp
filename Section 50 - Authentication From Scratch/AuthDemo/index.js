1540adc4406c05cae336d3b0b937a11b6060bc4e
Michiel Dijk <michiellof@gmail.com>
2020-12-18 15:47:23 +0100

    Lesson 497: Auth demo - registering

--- Section 50 - Authentication From Scratch/AuthDemo/index.js
+++ Section 50 - Authentication From Scratch/AuthDemo/index.js
@@ -1,12 +1,40 @@
 const express = require('express');
 const app = express();
 const User = require('./models/user');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+mongoose.connect('mongodb://localhost:27017/authDemo', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => {
+        console.log("Mongo connection open...");
+    })
+    .catch(err => {
+        console.log("MONGO CONNECTION ERROR!");
+        console.log(err);
+    });
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('this is the root page');
+});
+
 app.get('/register', (req, res) => {
     res.render('register');
+});
+
+app.post('/register', async (req, res) => {
+    const { password, username } = req.body;
+    const hash = await bcrypt.hash(password, 12);
+    const user = new User({
+        username,
+        password: hash
+    });
+    await user.save();
+    res.redirect('/');
 });
 
 app.get('/secret', (req, res) => {
