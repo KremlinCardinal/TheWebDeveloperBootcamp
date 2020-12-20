a2ee6cb9f340b38a817238ec8354b4d955541a66
Michiel Dijk <michiellof@gmail.com>
2020-12-20 14:23:48 +0100

    Lesson 508: Login routes

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -3,6 +3,7 @@ const { required } = require('joi');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
+const passport = require('passport');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
@@ -21,5 +22,14 @@ router.post('/register', catchAsync(async (req, res) => {
         res.redirect('register');
     }
 }));
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+});
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'Welcome back!');
+    res.redirect('/campgrounds');
+});
 
 module.exports = router;
