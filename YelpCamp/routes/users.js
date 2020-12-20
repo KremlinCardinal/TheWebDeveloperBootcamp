29e2006e9835165dbbe5e3cc08c33bdfdcd091f8
Michiel Dijk <michiellof@gmail.com>
2020-12-20 15:31:15 +0100

    Lesson 512: Fixing registration route

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -9,14 +9,19 @@ router.get('/register', (req, res) => {
     res.render('users/register');
 });
 
-router.post('/register', catchAsync(async (req, res) => {
+router.post('/register', catchAsync(async (req, res, next) => {
     try {
         const { email, username, password } = req.body;
         const user = new User({ email, username });
         const registeredUser = await User.register(user, password);
-        console.log(registeredUser);
-        req.flash('success', 'Welcome to YelpCamp!');
-        res.redirect('/campgrounds');
+        req.login(registeredUser, err => {
+            if (err) {
+                return next(err);
+            }
+            req.flash('success', 'Welcome to YelpCamp!');
+            res.redirect('/campgrounds');
+        });
+
     } catch (e) {
         req.flash('error', e.message);
         res.redirect('register');
