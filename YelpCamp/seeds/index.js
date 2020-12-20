6c173cb1d7f06f6700a9ab67060445533c91bfd6
Michiel Dijk <michiellof@gmail.com>
2020-12-20 16:09:52 +0100

    Lesson 514: Adding author to campground

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -24,6 +24,7 @@ const seedDB = async () => {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
+            author: '5fdf4d364505213724ee8a9b',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
