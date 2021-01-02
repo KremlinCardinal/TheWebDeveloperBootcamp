a6f783e54e2819cd2c589f9e3d7caee59a9b8fe6
Michiel Dijk <michiellof@gmail.com>
2021-01-02 20:24:18 +0100

    Lesson 538: Thumbnail virtual

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -2,14 +2,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const Review = require('./review');
 
+// https://res.cloudinary.com/kremlincardinal/image/upload/w_150/v1609523568/YelpCamp/tqcu5bryrg6jqri78n3u.jpg
+
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
