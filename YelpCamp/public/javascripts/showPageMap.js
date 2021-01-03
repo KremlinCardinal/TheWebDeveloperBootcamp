2907993472f99dd96c7b0a0cc8b306360039904e
Michiel Dijk <michiellof@gmail.com>
2021-01-03 19:44:12 +0100

    Lesson 545: Customizing map popup

--- YelpCamp/public/javascripts/showPageMap.js
+++ YelpCamp/public/javascripts/showPageMap.js
@@ -4,11 +4,17 @@ const campgroundObj = JSON.parse(campground);
 
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
     center: campgroundObj.geometry.coordinates, // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campgroundObj.geometry.coordinates)
+    .setPopup(
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campgroundObj.title}</h3><p>${campgroundObj.location}</p>`
+            )
+    )
     .addTo(map);
