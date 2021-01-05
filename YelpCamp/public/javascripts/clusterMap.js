0b9281b300f4face093055b29e115ca9d5b14ce2
Michiel Dijk <michiellof@gmail.com>
2021-01-05 19:13:35 +0100

    Lesson 549: Basic clustering campgrounds

--- YelpCamp/public/javascripts/clusterMap.js
+++ YelpCamp/public/javascripts/clusterMap.js
@@ -1,3 +1,8 @@
+const campgroundsObj = JSON.parse(campgrounds);
+
+console.log("campgrounds: ", campgrounds);
+console.log("campgroundsObj: ", campgroundsObj);
+
 mapboxgl.accessToken = mapToken;
 var map = new mapboxgl.Map({
     container: 'map',
@@ -14,8 +19,7 @@ map.on('load', function () {
         type: 'geojson',
         // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
         // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data:
-            'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        data: campgroundsObj,
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
