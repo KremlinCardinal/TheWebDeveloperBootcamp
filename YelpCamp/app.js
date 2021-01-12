10a2a65ee43e9b31f67057819a17eaa973326391
Michiel Dijk <michiellof@gmail.com>
2021-01-12 21:59:29 +0100

    Lesson 560: Mongo injection

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -14,6 +14,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const campgrounds = require('./routes/campgrounds');
 const reviews = require('./routes/reviews');
 const users = require('./routes/users');
@@ -40,6 +42,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize());
 
 const sessionConfig = {
     secret: 'veryverysecret',
